9566b95acff5f2e79234f04e22995d3a8d8ee3f4
Philippe Doucet Beaupré <doucetb2@illinois.edu>
2015-04-21 15:03:56 -0500

    CatastropheProcess and Zeroth order templates implemented, summary partially fixed

--- phyloSSA2.py
+++ phyloSSA2.py
@@ -1,20 +1,40 @@
 from sortedcontainers import SortedListWithKey, SortedList, SortedSet
 from random import sample, randint, uniform, random, seed, getstate
 from itertools import count, permutations, combinations, izip, chain
-from math import sqrt, log, exp, pi, floor, tanh
+from math import sqrt, log, exp, pi, floor, tanh, sin, cos
 from collections import namedtuple, defaultdict
 from sys import exit
 import time
 
+
+# You have to be careful here, object can proliferate when calling them
+# inline as parameters, creating new dictionary keys. Tuples both avoid
+# the problem but create new ones by being immutable.
+
 Point = namedtuple('Point', ['niche','age', 'vulnerability'])
 
+# class Point(object):
+#  	def __init__(self, niche, age, vulnerability):
+#  		self.niche = niche
+#  		self.age = age
+#  		self.vulnerability = vulnerability
+
+
+
+# class ProcessBin(object):
+# 	def __init__(self, binrate=0.0, processes_in_bin=SortedSet(key=lambda pr: pr.rate)):
+# 		self.binrate = binrate
+# 		self.processes_in_bin = processes_in_bin
+# 	def __del__(self):
+# 		self.processes_in_bin = None
 
 class ProcessBin(object):
-	def __init__(self, binrate=0.0, processes_in_bin=SortedSet(key=lambda pr: pr.rate)):
+	def __init__(self, binrate=0.0, processes_in_bin={}):
 		self.binrate = binrate
 		self.processes_in_bin = processes_in_bin
 	def __del__(self):
 		self.processes_in_bin = None
+
 
 class Parameters(object):
 	''' Contains program parameters'''
@@ -23,11 +43,12 @@ class Parameters(object):
 		self.fixed_seed = False
 		
 		# visualization = 'console', 'pretty', or 'pretty3d'
+		#self.visualization = 'pretty3d'
 		self.visualization = 'pretty3d'
 		
 		self.pptrait = 10.0
 
-		self.drawinterval = 10
+		self.drawinterval = 400
 		self.summaryinterval = 1000
 		self.recalibrateinterval = 200000
 
@@ -39,18 +60,20 @@ class Parameters(object):
 		# Stochastic rates and lattice parameters
 		self.birthrate = 1.0
 		self.deathrate = 0.5
-		self.intracomprate = 0.02
+		self.intracomprate = 0.01
 		self.nichestep = 1
-		self.intercomprate = 0.01
+		self.intercomprate = 0.005
 
 		self.nichemutationrate = 0.03
-		self.nicheKernel = [-1, 0, 1]
+		self.nicheKernel = [-1, 1]
 		self.nicheWidth = 21
 
 		self.vulnerabilitymutationrate = 0.03
 		#self.vulnstep = 1
-		self.vulnKernel = [-1, 0, 1]
-		self.vulnerabilityWidth = 11
+		self.vulnKernel = [-1, 1]
+		self.vulnerabilityWidth = 21
+
+		self.catastropherate = 0.01
 
 		# Probabily about smallest rate above, unless some
 		# processes have short-tailed kernels, in which case you
@@ -75,7 +98,7 @@ class Parameters(object):
 		self.muted_metals_palette = [[17,28,34],[36,61,66],[205,197,127],[237,220,204],[184,179,173]]
 
 		# Set the palette of your choosing
-		self.palette = self.muted_metals_palette
+		self.palette = self.dark_palette
 
 class BirthProcess(object):
 	''' The Birth process is a 1st order process sitting at a Point.\n
@@ -131,9 +154,10 @@ class DeathProcess(object):
 		self.lattice.annihilate(self.point)
 
 class CatastropheProcess(object):
-	def __init__(self, lattice, params):
+	def __init__(self, vuln, lattice, params):
 		self.order = 0
-		self.rate = 0
+		self.rate = params.catastropherate
+		self.vulnerability = vuln
 		self.lattice = lattice
 		self.params = params
 
@@ -146,13 +170,16 @@ class CatastropheProcess(object):
 
 	@staticmethod
 	def stencile(lattice, params):
-		pass
+		for vuln in xrange(params.vulnerabilityWidth):
+			yield vuln
 
 	def refresh_rate(self):
-		pass
+		self.rate = self.params.catastropherate
 
 	def do(self):
-		pass
+		vulnerable_points = [p for p in self.lattice.sites if p.vulnerability == self.vulnerability]
+		for p in vulnerable_points:
+			self.lattice.extinction(p)
 
 
 
@@ -297,6 +324,8 @@ class NicheNNInterCompetitionProcess(object):
 	def stencile(point, lattice, params):
 		# You MUST walk on everybody because all ages and other dimensions
 		# aside from niche might be impicated in a NN competitive interaction
+		# ... this is so ugly, there has to be a better way
+
 		for p2 in lattice.sites:
 			if ((point.niche + params.nichestep) % params.nicheWidth == p2.niche or (point.niche - params.nichestep) % params.nicheWidth == p2.niche) or (point.niche == p2.niche and point != p2):
 				yield (point, p2)
@@ -351,10 +380,13 @@ class Lattice(object):
 		self.total_n = 0
 
 		# Now attach 0-order processes to the lattice itself
-		for ztemplate in self.classes0th:
-			
-			zerothprocess = ztemplate()
-			self.attached_processes[0].append()
+		self.attached_processes[0] = []
+		for ZPrtemplate in self.classes0th:
+			for vuln in ZPrtemplate.stencile(self, self.params):
+				process = ZPrtemplate(vuln, self, self.params)
+				self.attached_processes[0].append(process)
+
+		self.meta.push(self.attached_processes[0])
 
 	def create(self, point):
 		self.total_n += 1
@@ -375,13 +407,13 @@ class Lattice(object):
 			self.sites[point] = 1
 			self.attached_processes[point] = []
 			# Generate and attach its 1st order processes...
-			for ptemplate in self.classes1st:
-				process = ptemplate(point, self, self.params)
+			for Prtemplate in self.classes1st:
+				process = Prtemplate(point, self, self.params)
 				self.attached_processes[point].append(process)
 			# ...also its 2nd order processes according to the stencile
-			for ptemplate in self.classes2nd:
-				for p1, p2 in ptemplate.stencile(point, self, self.params):
-					process = ptemplate(p1, p2, self, self.params)
+			for Prtemplate in self.classes2nd:
+				for p1, p2 in Prtemplate.stencile(point, self, self.params):
+					process = Prtemplate(p1, p2, self, self.params)
 					self.attached_processes[p1].append(process)
 					self.attached_processes[p2].append(process)
 
@@ -395,55 +427,45 @@ class Lattice(object):
 
 	def annihilate(self, point):
 		self.total_n -= 1
-		if point in self.sites.keys():
-			# Pull affected processes out of the meta process
-			affected = self.attached_processes[point]
-			self.meta.pull(affected)
-			# Annihilate and refresh their rates
+		if self.sites.has_key(point):
+			# # Annihilate and refresh their rates
 			self.sites[point] -= 1
-			for pr in affected:
-				pr.refresh_rate()
-			if self.sites[point] > 0:
-				# Push them back into th emeta process if the site is still active
-				#self.meta.push_processes([a for a in affected if a.rate > 0.0])
-				self.meta.push(affected)
-			else:
-				# Otherwise delete the point from the dictionary...
-				del self.sites[point]
-				# ...along with its attached processes
-				for pr in self.attached_processes[point]:
-					# but just before that, make sure that these processes'
-					# references lingering at point2 get cleaned first
-					if pr.order == 2:
-						if pr.point1 != point:
-							self.attached_processes[pr.point1].remove(pr)
-						if pr.point2 != point:
-							self.attached_processes[pr.point2].remove(pr)
-				# ...and delete these attached processes for real now
-				del self.attached_processes[point]
-				# They were pull, so nothing's left to push.
+			self.refresh_and_delete_attached(point)
 		else:
 			# So you're trying to annihilate at some emtpy site? Something went horribly wrong
 			raise ValueError('Orphan process, annihilation at empty site ' + repr(point))
 
-	def vulnerability_extinction(self, vuln):
-		for point in self.sites.keys():
-			if point.vulnerability == vuln:
-				affected = self.attached_processes[point]
-				self.meta.pull(affected)
-				self.sites[point] = 0
-				for pr in affected:
-					pr.refresh_rate()
-				del self.sites[point]
-				for pr in self.attached_processes[point]:
-					if pr.order == 2:
-						if pr.point1 != point:
-							self.attached_processes[pr.point1].remove(pr)
-						if pr.point2 != point:
-							self.attached_processes[pr.point2].remove(pr)
-				del self.attached_processes[point]
+	def extinction(self, point):
+		self.total_n -= self.sites[point]
+		self.sites[point] = 0
+		self.refresh_and_delete_attached(point)
 
-	# This is used by refresh_rate methods
+	def refresh_and_delete_attached(self, point):
+		affected = self.attached_processes[point]
+		self.meta.pull(affected)
+		for pr in affected:
+			pr.refresh_rate()
+		if self.sites[point] > 0:
+			# Push them back into the meta process if the site is still active
+			#self.meta.push_processes([a for a in affected if a.rate > 0.0])
+			self.meta.push(affected)
+		else:
+			# Otherwise delete the point from the dictionary...
+			del self.sites[point]
+			# ...along with its attached processes
+			for pr in self.attached_processes[point]:
+				# but just before that, make sure that these processes'
+				# references lingering at point2 get cleaned first
+				if pr.order == 2:
+					if pr.point1 != point:
+						self.attached_processes[pr.point1].remove(pr)
+					if pr.point2 != point:
+						self.attached_processes[pr.point2].remove(pr)
+			# ...and delete these attached processes for real now
+			del self.attached_processes[point]
+			# They were pull, so nothing's left to push.
+
+	# This is used by Process.refresh_rate methods
 	def n(self, point):
 		#if point in self.sites.keys():
 		if self.sites.has_key(point):
@@ -501,6 +523,12 @@ class Metaprocess(object):
 			self.push = self.push_binned_processes
 			self.step = self.step_compositionrejection
 
+	@property
+	def number_of_active_processes(self):
+	    return max(len(self._process_bag),
+	    	len(self._sorted_processes),
+	    	sum([len(b.processes_in_bin) for b in self._process_bins.values()]))
+	
 
 	# Used by direct Gillespie algorithm (simple accumulator)
 	def pull_processes(self, processes):
@@ -562,7 +590,6 @@ class Metaprocess(object):
 	# Random bag of processes used by the gillespie algorithm
 
 
-	# This might not work because the list of process is sorted
 	def step_gillespie(self):
 		if len(self._process_bag) > 0:
 			r1 = random()
@@ -634,6 +661,7 @@ class Metaprocess(object):
 
 		return residue
 
+
 	def step_compositionrejection(self):
 		residue = None
 		if len(self._process_bins) > 0:
@@ -669,9 +697,10 @@ class Metaprocess(object):
 
 def summary(old_time, new_time):
 	return ('processed = ' + repr(meta.processed) 
+		+ ' | time = ' + '%.1f'%meta._t
 		+ ' | total pop = ' + repr(lattice.total_n) 
 		+ ' | num. sps. = ' + repr(len(lattice.sites)) 
-		+ ' | active proc. = ' + repr(len(meta._sorted_processes)) 
+		+ ' | active proc. = ' + repr(meta.number_of_active_processes) 
 		+ ' | tries in 1K = ' + repr(meta.intervaltries) 
 		+ ' | avg. tries = ' + '%.1f'%(float(meta.intervaltries)/params.summaryinterval) 
 		#+ ' | min rate = ' + '%.2f'%(meta._sorted_processes[0].rate) 
@@ -685,7 +714,7 @@ if params.fixed_seed: seed(params.fixed_seed)
 
 meta = Metaprocess(params)
 
-lattice = Lattice(meta, [], [BirthProcess, DeathProcess, IntraCompetitionProcess, NicheStepMutationProcess], [NicheNNInterCompetitionProcess], params)
+lattice = Lattice(meta, [CatastropheProcess], [BirthProcess, DeathProcess, IntraCompetitionProcess, NicheStepMutationProcess, VulnerabilityStepMutationProcess], [NicheNNInterCompetitionProcess], params)
 #lattice = Lattice(meta, [], [BirthProcess, DeathProcess, IntraCompetitionProcess, VulnerabilityStepMutationProcess], [NicheNNInterCompetitionProcess], params)
 #lattice = Lattice(meta, [], [BirthProcess, DeathProcess, IntraCompetitionProcess], [], params)
 
@@ -696,11 +725,14 @@ lattice = Lattice(meta, [], [BirthProcess, DeathProcess, IntraCompetitionProcess
 # 	lattice.create(p1)
 # 	lattice.create(p4)
 
-for i in xrange(0,21,3):
-		lattice.create(Point(niche=i, age=0.0, vulnerability=0))
-		lattice.create(Point(niche=i, age=0.0, vulnerability=3))
-		lattice.create(Point(niche=i, age=0.0, vulnerability=6))
-		lattice.create(Point(niche=i, age=0.0, vulnerability=9))
+# for i in xrange(0,21,3):
+# 		lattice.create(Point(niche=i, age=0.0, vulnerability=0))
+# 		lattice.create(Point(niche=i, age=0.0, vulnerability=6))
+# 		lattice.create(Point(niche=i, age=0.0, vulnerability=12))
+# 		lattice.create(Point(niche=i, age=0.0, vulnerability=18))
+
+for i in xrange(20):
+	lattice.create(Point(niche=10.0, age=0.0, vulnerability=10.0))
 
 if params.visualization == 'pretty':
 	from pyprocessing import *
@@ -789,38 +821,44 @@ if params.visualization == 'pretty':
 elif params.visualization == 'pretty3d':
 	from pyprocessing import *
 	iwanthue = dict()
-	h = 0
+	h = 0.0
 
 	def setup():
 		size(1200, 840)
+		#size(1900,1000)
+		#size(displayWidth, displayHeight)
 		#frameRate(100)
 		background(0)
 		ellipseMode(CENTER)
 
 	def draw():
 		global h
-		for i in xrange(100):
+		for i in xrange(params.drawinterval):
 			residue = meta.step()
 
 		background(0)
 
-		h = max(h, sum(p.age for p in lattice.sites.iterkeys())/len(lattice.sites.keys()))
+		#h = max(h, sum(p.age for p in lattice.sites.iterkeys())/len(lattice.sites.keys()))
+		h = max(max(p.age for p in lattice.sites.iterkeys()), h)
 
-		camera(-40, -50, 30.0+h, 30.0, 20.0, 0+h, 0.0, 0.0, -1.0);
+		camera(-40, -50, -70+h, 30, 20, -40+h, 0, 0, 1);
 	
 		#translate(-100, -100, 0)
 
 		stroke(50)
 		strokeWeight(1)
-		line(0, 0, h, params.nicheWidth*params.pptrait, 0, h)
-		line(0, 0, h, 0, params.vulnerabilityWidth*params.pptrait, h)
+		line(0.0, 0.0, h, params.nicheWidth*params.pptrait, 0.0, h)
+		line(0.0, 0.0, h, 0.0, params.vulnerabilityWidth*params.pptrait, h)
+		line(0.0, params.vulnerabilityWidth*params.pptrait, h, params.nicheWidth*params.pptrait, params.vulnerabilityWidth*params.pptrait, h)
+		line(params.nicheWidth*params.pptrait, 0, h, params.nicheWidth*params.pptrait, params.vulnerabilityWidth*params.pptrait, h)
+
 
 		oldest = min(p.age for p in lattice.sites.iterkeys())
 		youngest = max(p.age for p in lattice.sites.iterkeys())
 
 		line(params.nicheWidth*params.pptrait, 0, -1, params.nicheWidth*params.pptrait, 0, oldest)
 		stroke(150,50,50)
-		strokeWeight(4)
+		strokeWeight(2)
 		line(params.nicheWidth*params.pptrait, 0, oldest, params.nicheWidth*params.pptrait, 0, youngest)
 		stroke(50)
 		strokeWeight(1)
@@ -828,28 +866,27 @@ elif params.visualization == 'pretty3d':
 
 		for i, (point, n) in enumerate(lattice.sites.iteritems()):
 
-			if iwanthue.has_key(point):
-				#stroke(*iwanthue[point])
-				fill(*iwanthue[point])
-			else:
+			if not iwanthue.has_key(point):
 				new_hue = sample(params.palette,1)[0]
 				iwanthue[point] = new_hue
-				#stroke(*iwanthue[point])
-				fill(*iwanthue[point])
 
-			
-			r = n
-			x = point.niche*params.pptrait
-			y = point.vulnerability*params.pptrait
-			z = point.age*1.0
+			r = 1.0*n
+			y = 1.0*point.niche*params.pptrait
+			x = 1.0*point.vulnerability*params.pptrait
+			z = 1.0*point.age
 
+			noFill()
 			stroke(*iwanthue[point])
 			line(x,y,h,x,y,z)
-			noStroke()
 
 			pushMatrix()
-			translate(x,y,z)
-			ellipse(0,0,r,r)
+			translate(x,y,h)
+			stroke(50)
+			ellipse(0.0, 0.0, r, r)
+			noStroke()
+			translate(0.0, 0.0, z-h)
+			fill(*iwanthue[point])
+			ellipse(0.0, 0.0, r, r)
 			popMatrix()
 			
 			# dh = 20
